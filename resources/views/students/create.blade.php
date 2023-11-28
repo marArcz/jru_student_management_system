27e25b9170f27d741a00eeb427d279beef774b57
= <=>
2023-11-28 15:28:23 -0500

    updates

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -8,36 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <p class="mb-3">
-
+                Create Student
             </p>
             <div class="card border-0 bg-white rounded-3">
                 <div class="card-body p-4">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Firstname</th>
-                                <th>Middlename</th>
-                                <th>Lastname</th>
-                                <th>Student ID</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($students as $student)
-                                <tr>
-                                    <td>{{ $student->firstname }}</td>
-                                    <td>{{ $student->middlename }}</td>
-                                    <td>{{ $student->lastname }}</td>
-                                    <td>{{ $student->student_id_no }}</td>
-                                    <td>
-                                        <div class="flex">
-
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                   <form action="" method="POST"></form>
                 </div>
             </div>
         </div>
